5d28002dc3d1b8759755e6137841bbd36409ad2b
Zhenkaaf <zhenkaaf@gmail.com>
2023-10-16 16:21:02 +0300

    get_all_boards_by_email

--- routes/boards.js
+++ routes/boards.js
@@ -1,13 +1,25 @@
 const router = require("express").Router();
-const boardSchema = require("../models/boardSchema");
+const BoardSchema = require("../models/boardSchema");
 //const bcrypt = require("bcrypt");
+
+router.get('/email/:email', async (req, res) => {
+  try {
+    console.log('req.params.email***', req.params.email);
+    const boards = await BoardSchema.find({ boardOwnerEmail: req.params.email });
+    res.status(200).json(boards);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 
 router.post("/newboard", async (req, res) => {
   try {
     console.log("creating newBoard");
     console.log('req.body---', req.body);
     const { boardTitle, columns, boardOwnerEmail } = req.body;
-    const newBoard = new boardSchema({ boardTitle, columns, boardOwnerEmail });
+    const newBoard = new BoardSchema({ boardTitle, columns, boardOwnerEmail });
     const board = await newBoard.save();
     res.set("Access-Control-Allow-Origin", "*");
     res.set("Access-Control-Allow-Methods", "POST");
